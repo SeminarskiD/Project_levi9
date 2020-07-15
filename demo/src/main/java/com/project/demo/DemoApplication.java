b2c67e14ccaaea7be8083ce62c8755d8bf985e07
Aki-Coder <anchicabeybe@gmail.com>
2020-07-15 11:28:43 +0200

    novo

--- demo/src/main/java/com/project/demo/DemoApplication.java
+++ demo/src/main/java/com/project/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.project.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- demo/src/main/java/com/project/demo/controller/AuthController.java
+++ demo/src/main/java/com/project/demo/controller/AuthController.java
@@ -1,8 +1,8 @@
 package com.project.demo.controller;
 
-import java.util.HashSet;
+
 import java.util.List;
-import java.util.Set;
+
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -20,13 +20,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.demo.model.EUloga;
-import com.project.demo.model.KorisnikEntity;
-import com.project.demo.model.UlogaEntity;
+
 import com.project.demo.payload.request.LoginRequest;
-import com.project.demo.payload.request.SingupRequest;
+
 import com.project.demo.payload.response.JwtResponse;
-import com.project.demo.payload.response.MessageResponse;
 import com.project.demo.repository.KorisnikRepository;
 import com.project.demo.repository.UlogaRepository;
 import com.project.demo.security.jwt.JwtUtils;
@@ -72,53 +69,5 @@ public class AuthController {
 												 roles));
 	}
 
-	@PostMapping("/signup")
-	public ResponseEntity<?> registerUser(@Valid @RequestBody SingupRequest signUpRequest) {
-		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-			return ResponseEntity
-					.badRequest()
-					.body(new MessageResponse("Greska: Korisnicko ime je zauzeto!"));
-		}
-
-		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-			return ResponseEntity
-					.badRequest()
-					.body(new MessageResponse("Error: Email je zauzet!"));
-		}
-
-		// Kreiranje novog naloga
-		KorisnikEntity user = new KorisnikEntity(signUpRequest.getUsername(), 
-							 signUpRequest.getEmail(),
-							 encoder.encode(signUpRequest.getPassword()));
-
-		Set<String> strRoles = signUpRequest.getRole();
-		Set<UlogaEntity> roles = new HashSet<>();
-
-		if (strRoles == null) {
-			UlogaEntity userRole = roleRepository.findByName(EUloga.ROLE_USER)
-					.orElseThrow(() -> new RuntimeException("Greska: Uloga nije pronadjenja ."));
-			roles.add(userRole);
-		} else {
-			strRoles.forEach(role -> {
-				switch (role) {
-				case "admin":
-					UlogaEntity adminRole = roleRepository.findByName(EUloga.ROLE_ADMIN)
-							.orElseThrow(() -> new RuntimeException("Greska: Uloga nije pronadjenja "));
-					roles.add(adminRole);
-
-					break;
-
-				default:
-					UlogaEntity userRole = roleRepository.findByName(EUloga.ROLE_USER)
-							.orElseThrow(() -> new RuntimeException("Greska: Uloga nije pronadjenja "));
-					roles.add(userRole);
-				}
-			});
-		}
-
-		user.setRoles(roles);
-		userRepository.save(user);
-
-		return ResponseEntity.ok(new MessageResponse("Korisnik je uspesno registrovan"));
-	}
+	
 }
